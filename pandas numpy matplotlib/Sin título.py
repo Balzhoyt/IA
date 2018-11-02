d1f5319066bedb69bd1de56b25c269a2605cdfd9
Balzhoyt <balzhoyt@hotmail.com>
2018-11-02 10:31:40 -0600

    subiendo repositorio
    subiendo repositorio

---
+++ pandas numpy matplotlib/Sin título.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 09:40:58 2018
+
+@author: Balzhoyt
+"""
+import numpy as np
+import pandas as pd
+df=pd.read_csv("D:\\Maestria\\Inteligencia Artificial\\auto autonomo\\CarVideos.xls")
+df.head()
